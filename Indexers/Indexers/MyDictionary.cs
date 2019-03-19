213a43741256e512972853a2768e731d63aab78a
eugene-306 <jkiselev2411@gmail.com>
2019-03-19 13:43:49 +0600

    start 3 lab

---
+++ Indexers/Indexers/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    public class MyDictionary<K, V>
+        where K : struct
+        where V : struct
+    {
+
+        public MyDictionary()
+        {
+            keys = new HashSet<K>();
+            values = new HashSet<V>();
+        }
+        private HashSet<K> keys;
+        private HashSet<V> values;
+        public V this[K key]
+        {
+            get {
+                var index = keys.ToList().IndexOf(key);
+                return index < 0 ? default(V) : values.ToList()[index];
+            }
+            set
+            {
+                keys.Add(key);
+                values.Add(value);
+            }
+        }
+    }
+}
---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entity = new MyByteArray(15);
+            entity.Print();
+            Console.WriteLine();
+            entity.Fill();
+            entity.Print();
+            Console.WriteLine("\n\n-------------------\n\n");
+
+            entity[5] = 55;
+            entity[6] = 66;
+            entity[7] = 77;
+
+
+            for(int i = 0; i < entity.Length; i++)
+            {
+                Console.WriteLine("["+i+"] = " + entity[i]);
+            }
+
+            var dict = new MyDictionary<int,int>();
+            dict[15] = 35;
+            dict[25] = 45;
+            dict[15] = 100;
+
+            Console.WriteLine("\n\n-------------------\n\n");
+            Console.WriteLine(dict[15]);
+            Console.WriteLine(dict[25]);
+            Console.WriteLine(dict[35]);
+            Console.WriteLine(dict[100]);
+            Console.ReadKey();
+        }
+    }
+}
