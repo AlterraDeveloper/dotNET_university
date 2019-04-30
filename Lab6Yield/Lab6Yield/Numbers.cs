7f1dc6849558ca327afc1ef131f9c78addc6b14c
Eugene Kiselev <jkiselev2411@gmail.com>
2019-04-30 19:13:16 +0600

    lab6 done

--- Lab6Yield/Lab6Yield/Numbers.cs
+++ Lab6Yield/Lab6Yield/Numbers.cs
@@ -8,18 +8,9 @@ using System.Threading.Tasks;
 
 namespace Lab6Yield
 {
-    public class Numbers : IEnumerable<int>
+    public static class Numbers
     {
-        private int startVal, step, qty;
-
-        public Numbers(int startVal, int step, int qty)
-        {
-            this.startVal = startVal;
-            this.step = step;
-            this.qty = qty;
-        }
-
-        public IEnumerator<int> GetEnumerator()
+        public static IEnumerable<int> GetSequence(int startVal, int step, int qty)
         {
             for (int i = 0; i < qty; i++)
             {
@@ -27,9 +18,109 @@ namespace Lab6Yield
             }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
+        public static IEnumerable<int> GetInfinitySequence()
         {
-            return GetEnumerator();
+            for (int i = 0; ; i++)
+            {
+                yield return i;
+            }
+        }
+
+        public static IEnumerable<int> GetInfinityRandomSequence()
+        {
+            for (int i = 0; ; i++)
+            {
+                yield return new Random(i).Next(0, 100);
+            }
+        }
+
+        public static IEnumerable<int> TakeN(IEnumerable<int> seq, int n)
+        {
+            int i = 0;
+            foreach (var num in seq)
+            {
+                if (i != n)
+                {
+                    i++;
+                    yield return num;
+                }
+                else yield break;
+
+            }
+        }
+
+        public static IEnumerable<int> GetFibonachiSequence()
+        {
+            int first = 0;
+            yield return first;
+            int second = 1;
+            yield return second;
+
+            for (; ; )
+            {
+                var current = first + second;
+                first = second;
+                second = current;
+                yield return current;
+            }
+        }
+
+        public static IEnumerable<int> GetLoopingSequence(IEnumerable<int> seq)
+        {
+            for (; ; )
+            {
+                foreach (var num in seq)
+                {
+                    yield return num;
+                }
+            }
+        }
+
+        public static IEnumerable<Tuple<int,int>> GetPermutations(IEnumerable<int> seq1, IEnumerable<int> seq2)
+        {
+            foreach (var num1 in seq1)
+            {
+                foreach (var num2 in seq2)
+                {
+                    yield return Tuple.Create(num1, num2);
+                }
+            }
+        }
+
+        public static IEnumerable<Tuple<int, int>> GetTuples(IEnumerable<int> seq1, IEnumerable<int> seq2)
+        {
+            List<int> seqList1 = new List<int>(seq1);
+            List<int> seqList2 = new List<int>(seq2);
+
+            for (int i = 0; i < Math.Min(seqList1.Count,seqList2.Count); i++)
+            {
+                yield return Tuple.Create(seqList1[i], seqList2[i]);
+            }
+        }
+
+        public static IEnumerable<int> GetSequenceOfEvens(IEnumerable<int> seq)
+        {
+            foreach(var num in seq)
+            {
+                if (num % 2 == 0) yield return num;
+            }
+        }
+
+        public static IEnumerable<int> GetSquares(IEnumerable<int> seq)
+        {
+            foreach (var num in seq)
+            {
+                yield return (int)Math.Pow(num,2);
+            }
+        }
+
+        public static IEnumerable<Tuple<int, int>> GetTuplesOfNearestElements(IEnumerable<int> seq)
+        {
+            List<int> seqList = new List<int>(seq);
+            for (int i = 0; i < seqList.Count-1; i++)
+            {
+                yield return Tuple.Create(seqList[i], seqList[i + 1]);
+            }
         }
     }
 }
--- Lab6Yield/Lab6Yield/Program.cs
+++ Lab6Yield/Lab6Yield/Program.cs
@@ -12,11 +12,65 @@ namespace Lab6Yield
 
         static void Main(string[] args)
         {
-            Numbers numbers = new Numbers(7,3,5);
-            foreach (var num in numbers)
+            foreach (var num in Numbers.GetSequence(50,10,7))
             {
-                Console.Out.WriteLine("num = {0}", num);
+                Console.Out.Write(num + " ");
             }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.TakeN(Numbers.GetInfinitySequence(),20))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.TakeN(Numbers.GetInfinityRandomSequence(), 10))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.TakeN(Numbers.GetFibonachiSequence(), 10))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.TakeN(Numbers.GetLoopingSequence(Numbers.TakeN(Numbers.GetFibonachiSequence(),4)), 10))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.GetPermutations(Numbers.GetSequence(1,1,2),Numbers.GetSequence(10,5,3)))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.GetSequenceOfEvens(Numbers.TakeN(Numbers.GetFibonachiSequence(), 10)))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.GetSquares(Numbers.GetSequence(10,1,5)))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.GetTuplesOfNearestElements(Numbers.GetSequence(1, 1, 5)))
+            {
+                Console.Out.Write(num + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var num in Numbers.GetTuples(Numbers.GetSequence(1, 1, 3), Numbers.GetSequence(4, 1, 3)))
+            {
+                Console.Out.Write(num + " ");
+            }
+
             Console.ReadKey();
         }
     }
